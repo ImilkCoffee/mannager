e3162457066adf6dd9e95ae9cb532a7f51525ba0
ImilkCoffee <529301328@qq.com>
2019-04-03 16:36:25 +0800

    统一错误页面完成

---
+++ src/components/error.vue
@@ -0,0 +1,41 @@
+<template >
+<div class='box'>
+    <div class="msg">
+    <p class='error'>404</p>
+    <span>对不起!你输入的网址找不到了!</span>
+    </div> 
+</div>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss'>
+html,body{
+    padding: 0;
+    margin:0;
+    .box{
+    background: url('../assets/psb1.jpg')no-repeat center/cover;
+    color:aliceblue;
+    height: 100%;
+    font-family: '微软雅黑';
+.msg{
+    position: absolute;
+    top:150px;
+    left: 200px;
+    .error{
+        font-size: 80px;
+        margin-left: 40px;
+        color: blue;
+    }
+}
+}
+}
+
+
+
+</style>
